7c6b83f819bf1e5b6b20fc3e07d2f02e1fe5f882
Shrihari <smurlidharan3@gatech.edu>
2013-04-03 13:02:36 -0400

    Shrihari:Corrected some unmatched html tags

--- screens/startadding.php
+++ screens/startadding.php
@@ -1,39 +1,36 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head>
-    <title>Pathway Mapper</title>
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />
-    <script src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
-    <script src="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.js"></script>
+        <title>Pathway Mapper</title>
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />
+        <script src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.js"></script>
 
 
-</head>
-<body> 
-<!-- Home -->
-<div data-role="page" id="page1">
-    <div data-role="content">
-        <div data-role="fieldcontain">
-		 <form action="routing.php" method="POST">
-            <fieldset data-role="controlgroup">
-                <label for="dest">
-                </label>
-                <input name="dest"  placeholder="" value="Enter the destination"
-                type="text" onclick="this.value=''" >
-				<input name="source"  placeholder="" value="Enter the start locaion"
-                type="text" onfocus="this.value=''">
-            </fieldset>
+    </head>
+    <body>
+        <!-- Home -->
+        <div data-role="page" id="page1">
+            <div data-role="content">
+                <div data-role="fieldcontain">
+                    <form action="routing.php" method="POST">
+                        <fieldset data-role="controlgroup">
+                            <label for="dest">
+                            </label>
+                            <input name="dest"  placeholder="" value="Enter the destination"
+                                   type="text" onclick="this.value=''" />
+                            <input name="source"  placeholder="" value="Enter the start locaion"
+                                   type="text" onfocus="this.value=''"/>
+                        </fieldset>
+                        <input type="submit" value="Start"/>
+                    </form>
+                    <a data-role="button" rel="external" href="homepage.html">
+                        Cancel
+                    </a>
+                </div>
+
+            </div>
         </div>
-            <input type="submit" value="Start">
-        </a>
-		</form>
-		
-       
-		
-        <a data-role="button" rel="external" href="homepage.html">
-            Cancel
-        </a>
-    </div>
-</div>
 
-</body>
+    </body>
 </html>
